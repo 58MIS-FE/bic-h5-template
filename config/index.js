ec8b36467253d1b992ac8c6f72af85220cdb7d69
zhangzhenonline <214464812@qq.com>
2018-03-06 22:19:58 +0800

    gulp

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ var path = require('path')
 
 module.exports = {
    port : 9090,
-   host : '10.252.55.70',  //默认是localhost 
+   host : '192.168.6.105',  //默认是localhost
    srcPath : path.join(__dirname, '../src'),
    cssStyle: '' , //less sass  默认 " " 为_style
    uglifyMap: false    //是否压缩
